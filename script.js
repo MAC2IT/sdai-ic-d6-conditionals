bd0f5297335a7258fbd9350ce633abc6ad63048e
MAC2IT <sourcefullresources@gmail.com>
2024-11-07 04:49:39 +0000

    added ages

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // all code for this lab to be completed in this file
+const currentTemperture = 72;
+
+if (currentTemperture > 32) {
+    console.log("It's not freezing... yippie")
+} else {
+    console.log("It's not freezing thank God")
+}
+
+
+let age1 = 42
+let age2 = 34
+
+if (age1 > age2) {
+    console.log("Luv I'm older than you haha")
+} else if (age1 < age2) {
+    //younger
+    console.log("Age over beauty cutie")
+} else if (age1 === age2) {
+    //equal age
+    console.log("Hot diggity dog we're the same age")
+} else {
+    console.log("Sweetheart you put in the wrong info")
+}
